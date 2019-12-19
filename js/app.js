d5182c6ee58ec1e29fb9e41d6b9e9bf9b5850454
Abdulkhaliq <afolaa27@uwgb.edu>
2019-12-19 14:53:14 -0600

    class with constructor properties setup and object,display view set up

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log("Tamagachi-App");
+
+class Tamagachi {
+	constructor (name){
+		let hunger = 0
+		let sleepiness = 0
+		let boredom = 0
+		let age = 0
+		let petName = name;
+
+	}
+}
+
+const game = {
+
+	time : 0,
+
+	startGame (){
+
+	}
+
+
+
+
+
+
+
+}
