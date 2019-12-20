5cc3e48870268a6fad35cede4bec3331b6c8e356
Abdulkhaliq <afolaa27@uwgb.edu>
2019-12-20 09:59:57 -0600

    time and buttons updated to functions as they should

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const game = {
 	time : 0,
 	timerID: "",
 	hunger:0,
-	sleepiness:0,
+	sleeps:0,
 	boredom:0,
 	age:0,
 	roundC: 1000,
@@ -21,11 +21,7 @@ const game = {
 
 	startGame (){
 		this.addPet()
-		this.age()
 		this.timeInterval()
-		
-
-
 	},
 
 	
@@ -48,50 +44,95 @@ const game = {
 		//console.log(locateBack);
 		locateDisp.style.backgroundImage = "url(https://media.giphy.com/media/i0W1wW0VqgsPS/giphy.gif)"
 
-
 		
 	},
 
+	printAttrValue(){
+		let petLocateHunger = document.querySelector('#hunger')
+		petLocateHunger.innerText = this.hunger
+
+		let petLocateBoredom = document.querySelector('#boredomess')
+		petLocateBoredom.innerText = this.boredom
+
+		let petLocateSleepiness = document.querySelector('#sleepiness')
+		petLocateSleepiness.innerText = this.sleeps
+
+		let ageVal = document.querySelector("#age")
+		roundC = 10000
+		ageVal.innerText = this.age
+		
+		this.killpetCheck()
+		
+
+	},
+
+	
+	feed(){
+		let locateDisp = document.querySelector('#displayGif')
+		//console.log(locateBack);
+		locateDisp.style.backgroundImage = "url(https://media.giphy.com/media/KirK3WZtkmfaE/giphy.gif)"
+		
+		let statusUdate = document.querySelector('#status')
+		statusUdate.innerText =" You Fed An Agent "
+		this.hunger--
+		let petLocateHunger = document.querySelector('#hunger')
+		petLocateHunger.innerText = this.hunger
+	},
+	sleep(){
+
+		let locateDisp = document.querySelector('#displayGif')
+		//console.log(locateBack);
+		locateDisp.style.backgroundImage = "url(https://media.giphy.com/media/LgcDsIX4q6LiU/giphy.gif)"
+
+		
+		let statusUdate = document.querySelector('#status')
+		statusUdate.innerText =" Agent Hibenating "
+
+		let petLocateSleepiness = document.querySelector('#sleepiness')
+		this.sleeps--
+		petLocateSleepiness.innerText = this.sleeps
+
+	},
 	timeInterval(time){
 		this.timerID = setInterval(() => {
 			this.time++
-			if(this.time > 11){
-				
-				
-				clearInterval(this.timerID)
+
+			if (this.time % 6 ===0){
+				this.age++
+
 			}
+			if (this.time % 3 === 0 ){	
+				this.hunger++
+			}
+			if (this.time % 6 === 0 ){	
+				this.sleeps++
+			}	
+			if (this.time % 2 === 0 ){	
+				this.boredom++
+			}	
+			
 			else {
 				
 				this.printAttrValue()
 			}
-
 			
-		},this.time)
+		},this.roundC)
 		
 	},
-	printAttrValue(){
-		this.age()
+	killpetCheck(){
+
+		if (this.sleepiness === 10 || this.hunger ===10 || this.boredom === 10){
+			clearInterval(this.timerID)
+			let locateBack = document.querySelector('#displayGif')
+			let statusUdate = document.querySelector('#status')
+			statusUdate.innerText =" You killed An Agent " 
 			
-		//console.log(ageVal.innerText);
+			console.log(locateBack);
+			locateBack.style.backgroundImage = "url(https://media.giphy.com/media/41xheAhKk10i20ymQt/giphy.gif)"
 
-	},
-
-	age(){
-		let ageVal = document.querySelector("#age")
-		ageVal.innerText = this.time
-		const ageTime = 1000
-		this.timeInterval(ageTime)
-
-	},
-	feed(){
-		const feedTime = 1000
-		this.setInterval(feedTime)
-
+		// }
 	}
-
-
-
-	
+}
 
 }
 
@@ -100,22 +141,26 @@ $('.addPetButton'). on('click', ()=>{
 	game.startGame()
 })
 $('.feed'). on('click',() => {
-
+	game.feed()
 })
 $('.sleep'). on('click',() => {
+	game.sleep()
 
 })
 $('.lights'). on('click',() => {
+	let statusUdate = document.querySelector('#status')
+	statusUdate.innerText =" Lights Off"
 	
 	let locateBack = document.querySelector('#displayGif')
 	console.log(locateBack);
-	locateBack.style.backgroundColor = "rgba(255,255,255,0.5"
+	// locateBack.style.backgroundColor = "rgba(255,255,255,0.5"
+	locateBack.style.opacity = ".1"
 	//locateBack.backGroundColor = 'yellow'
 })
 $('.lights'). on('dblclick',() => {
 	
 	let locateBack = document.querySelector('#displayGif')
 	console.log(locateBack);
-	locateBack.style.backgroundColor = "transparent"
+	locateBack.style.opacity = "1.0"
 	
 })
